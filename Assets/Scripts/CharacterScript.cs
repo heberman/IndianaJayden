67cea34a2004c4450fb3bc24990925808eaac703
EvanJSpringer <81826544+EvanJSpringer@users.noreply.github.com>
2023-03-08 20:09:07 -0800

    Updated Grapple forces

--- Assets/Scripts/CharacterScript.cs
+++ Assets/Scripts/CharacterScript.cs
@@ -9,11 +9,7 @@ public class CharacterScript : MonoBehaviour
     public GrappleScript grappleScript;
     public bool grapple;
 
-    public float acceleration = 1f;
-    public float decceleration = -1f;
-    public float runSpeed = 0.001f;
-
-    //public float runSpeed = 60f;
+    public float runSpeed = 60f;
     public float horizontalMove = 0f;
     private bool facingRight = false;
     public float verticalMove = 0f;
@@ -87,15 +83,14 @@ public class CharacterScript : MonoBehaviour
         if (!grapple) {
             rigidBody.velocity = new Vector2(horizontalMove * Time.fixedDeltaTime, rigidBody.velocity.y);
         }
-        //ignore this for now, I tried to make movement work when grappling
-
-        //else
-        //{
-        //  float speedDif = horizontalMove - rigidBody.velocity.x;
-        //    float acc = (Mathf.Abs(horizontalMove) > 0.01f) ? acceleration : decceleration;
-        //    float movement = Mathf.Pow(Mathf.Abs(speedDif) * acc, 0.9f) * Mathf.Sign(speedDif);
-        //    rigidBody.AddForce(movement * Vector2.right);
-        //}
+        else
+        {
+            float speedDif = horizontalMove - rigidBody.velocity.x;
+            float acc = (Mathf.Abs(horizontalMove) > 0.01f) ? 0.01f : -0.01f;
+            //float movement = Mathf.Pow(Mathf.Abs(speedDif) * acc, 1f) * Mathf.Sign(speedDif) * 0.001f;
+            float movement = speedDif * acc;
+            rigidBody.AddForce(movement * Vector2.right);
+        }
     }
 
     public void Flip()
